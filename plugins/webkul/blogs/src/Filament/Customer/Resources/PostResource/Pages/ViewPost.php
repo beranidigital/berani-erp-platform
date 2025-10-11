892c5d8bd7c658757319e4b4c1fc9f72284edf5d
RizkiAkhmal <you@example.com>
2025-10-11 10:14:42 +0700

    Fix [Blog Page]: Uncategorized Blog Bug

--- plugins/webkul/blogs/src/Filament/Customer/Resources/PostResource/Pages/ViewPost.php
+++ plugins/webkul/blogs/src/Filament/Customer/Resources/PostResource/Pages/ViewPost.php
@@ -12,6 +12,32 @@ class ViewPost extends ViewRecord
 
     protected string $view = 'blogs::filament.customer.resources.post.pages.view-record';
 
+    public function mountParentRecord(): void
+    {
+        if ($this->parentRecord) {
+            return;
+        }
+
+        $parentResourceRegistration = static::getResource()::getParentResourceRegistration();
+
+        if (! $parentResourceRegistration) {
+            return;
+        }
+
+        $parameters = request()->route()->parameters();
+        $paramName = $parentResourceRegistration->getParentRouteParameterName();
+
+        // If the parent route parameter (e.g., category) is missing or null,
+        // skip resolving the parent record to avoid passing null to Filament.
+        if (! array_key_exists($paramName, $parameters) || $parameters[$paramName] === null) {
+            return;
+        }
+
+        $this->parentRecord = $this->resolveParentRecord($parameters);
+
+        $this->authorizeParentRecordAccess();
+    }
+
     public function getBreadcrumbs(): array
     {
         return [];
